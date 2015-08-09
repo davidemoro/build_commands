5a0ce59b1967abd726b9828462048bd89693f8ac
Davide Moro <davide.moro@gmail,.com>
2015-08-10 00:41:19 +0200

    update test bower (instance_dir no more mandatory)

--- build_commands/tests/test_bower.py
+++ build_commands/tests/test_bower.py
@@ -22,8 +22,8 @@ class TestBowerTest:
         with pytest.raises(DistutilsArgError):
             cmd.finalize_options()
 
-    def test_finalize_options_no_instance_dir(self):
-        """ instance_dir not found """
+    def test_finalize_options_no_instance_dir_fail(self):
+        """ instance_dir provided not found """
         from setuptools.dist import Distribution
         dist = Distribution(
             dict(name='foo',
@@ -34,13 +34,33 @@ class TestBowerTest:
         dist.script_name = 'setup.py'
         from build_commands import BowerCommand
         cmd = BowerCommand(dist)
-        from distutils.errors import DistutilsArgError
+        import tempfile
+        cmd.instance_dir = 'notfound'
         import mock
         with mock.patch('build_commands.bower.find_executable') \
                 as find_executable:
             find_executable.return_value = '/tmp/bower'
+            from distutils.errors import DistutilsArgError
             with pytest.raises(DistutilsArgError):
                 cmd.finalize_options()
+
+    def test_finalize_options_no_instance_dir(self):
+        """ instance_dir not found (not mandatory) """
+        from setuptools.dist import Distribution
+        dist = Distribution(
+            dict(name='foo',
+                 packages=['foo'],
+                 use_2to3=True,
+                 version='0.0',
+                 ))
+        dist.script_name = 'setup.py'
+        from build_commands import BowerCommand
+        cmd = BowerCommand(dist)
+        import mock
+        with mock.patch('build_commands.bower.find_executable') \
+                as find_executable:
+            find_executable.return_value = '/tmp/bower'
+            cmd.finalize_options()
 
     def test_finalize_options_ok(self):
         """ instance_dir and command ok """
@@ -94,6 +114,37 @@ class TestBowerTest:
         expected = ['bower', 'install', cmd.instance_dir]
         spawn_mock.assert_called_once_with(expected)
 
+    def test_run_ok_no_instance_dir(self):
+        """ Assert spawn is called with the right parameters """
+        from setuptools.dist import Distribution
+        dist = Distribution(
+            dict(name='foo',
+                 packages=['foo'],
+                 use_2to3=True,
+                 version='0.0',
+                 ))
+        dist.script_name = 'setup.py'
+        from build_commands import BowerCommand
+        cmd = BowerCommand(dist)
+        import tempfile
+        import mock
+        with mock.patch('build_commands.bower.find_executable') \
+                as find_executable:
+            find_executable.return_value = '/tmp/bower'
+            cmd.finalize_options()
+
+        spawn_mock = mock.MagicMock()
+        cmd.spawn = spawn_mock
+        import sys
+        old_stdout = sys.stdout
+        try:
+            cmd.run()
+        finally:
+            sys.stdout = old_stdout
+
+        expected = ['bower', 'install']
+        spawn_mock.assert_called_once_with(expected)
+
     def test_run_ok_production(self):
         """ Assert spawn is called with the right parameters """
         from setuptools.dist import Distribution
@@ -125,4 +176,36 @@ class TestBowerTest:
             sys.stdout = old_stdout
 
         expected = ['bower', 'install', cmd.instance_dir, '-p']
+        spawn_mock.assert_called_once_with(expected)
+
+    def test_run_ok_production_no_instance_dir(self):
+        """ Assert spawn is called with the right parameters """
+        from setuptools.dist import Distribution
+        dist = Distribution(
+            dict(name='foo',
+                 packages=['foo'],
+                 use_2to3=True,
+                 version='0.0',
+                 ))
+        dist.script_name = 'setup.py'
+        from build_commands import BowerCommand
+        cmd = BowerCommand(dist)
+        import tempfile
+        import mock
+        with mock.patch('build_commands.bower.find_executable') \
+                as find_executable:
+            find_executable.return_value = '/tmp/bower'
+            cmd.finalize_options()
+
+        spawn_mock = mock.MagicMock()
+        cmd.spawn = spawn_mock
+        cmd.production = True
+        import sys
+        old_stdout = sys.stdout
+        try:
+            cmd.run()
+        finally:
+            sys.stdout = old_stdout
+
+        expected = ['bower', 'install', '-p']
         spawn_mock.assert_called_once_with(expected)
