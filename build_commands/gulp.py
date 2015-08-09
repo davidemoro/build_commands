a67cd93755ff855d9a52d2d5df4f6c7ee9a09587
Davide Moro <davide.moro@gmail,.com>
2015-08-10 00:31:27 +0200

    fix indenting

--- build_commands/gulp.py
+++ build_commands/gulp.py
@@ -16,38 +16,38 @@ class GulpCommand(Command):
     ]
 
     def initialize_options(self):
-      self.executable = 'gulp'
-      self.instance_dir = None
-      self.gulpfile = None
-      self.gulpfile_path = None
+        self.executable = 'gulp'
+        self.instance_dir = None
+        self.gulpfile = None
+        self.gulpfile_path = None
   
     def finalize_options(self):
-      executable = find_executable(self.executable)
-      if not executable:
-          raise DistutilsArgError(
-              "{0} not found. You must specify --executable or -e"
-              " with the gulp path".format(self.executable)
-              )
-      if self.instance_dir is None or not os.path.isdir(self.instance_dir):
-          raise DistutilsArgError(
-              "project dir {0} not found."
-              " You must specify --instance_dir or -p"
-              " with the project instance_dir".format(self.instance_dir)
-              )
-      if self.gulpfile is None:
-          raise DistutilsArgError(
-              "gulpfile {0} not provided."
-              " You must specify --gulpfile or -g"
-              " with the gulpfile name".format(self.gulpfile_path)
-              )
-      else:
-          self.gulpfile_path = os.path.join(self.instance_dir, self.gulpfile)
-          if self.gulpfile_path is None or not os.path.isfile(self.gulpfile_path):
-              raise DistutilsArgError(
-                  "gulpfile {0} not found."
-                  " You must specify --gulpfile or -g"
-                  " with the gulpfile name".format(self.gulpfile_path)
-                  )
+        executable = find_executable(self.executable)
+        if not executable:
+            raise DistutilsArgError(
+                "{0} not found. You must specify --executable or -e"
+                " with the gulp path".format(self.executable)
+                )
+        if self.instance_dir is None or not os.path.isdir(self.instance_dir):
+            raise DistutilsArgError(
+                "project dir {0} not found."
+                " You must specify --instance_dir or -p"
+                " with the project instance_dir".format(self.instance_dir)
+                )
+        if self.gulpfile is None:
+            raise DistutilsArgError(
+                "gulpfile {0} not provided."
+                " You must specify --gulpfile or -g"
+                " with the gulpfile name".format(self.gulpfile_path)
+                )
+        else:
+            self.gulpfile_path = os.path.join(self.instance_dir, self.gulpfile)
+            if self.gulpfile_path is None or not os.path.isfile(self.gulpfile_path):
+                raise DistutilsArgError(
+                    "gulpfile {0} not found."
+                    " You must specify --gulpfile or -g"
+                    " with the gulpfile name".format(self.gulpfile_path)
+                    )
   
     def run(self):
       command = '{0} build --base {1} --gulpfile {2}'.format(
--- build_commands/npm.py
+++ build_commands/npm.py
@@ -15,29 +15,29 @@ class NpmCommand(Command):
     ]
 
     def initialize_options(self):
-      self.executable = 'npm'
-      self.instance_dir = None
+        self.executable = 'npm'
+        self.instance_dir = None
   
     def finalize_options(self):
-      command = find_executable(self.executable)
-      if not command:
-          raise DistutilsArgError(
-              "{0} not found. You must specify --executable or -e"
-              " with the npm instance_dir".format(self.executable)
-              )
-      if self.instance_dir is None or not os.path.isdir(self.instance_dir):
-          raise DistutilsArgError(
-              "project dir {0} not found."
-              " You must specify --instance_dir or -p"
-              " with the project instance_dir".format(self.instance_dir)
-              )
+        command = find_executable(self.executable)
+        if not command:
+            raise DistutilsArgError(
+                "{0} not found. You must specify --executable or -e"
+                " with the npm instance_dir".format(self.executable)
+                )
+        if self.instance_dir is None or not os.path.isdir(self.instance_dir):
+            raise DistutilsArgError(
+                "project dir {0} not found."
+                " You must specify --instance_dir or -p"
+                " with the project instance_dir".format(self.instance_dir)
+                )
   
     def run(self):
-      command = '{0} install --prefix {1} {1}'.format(
-          self.executable,
-          self.instance_dir,
-          )
-      self.announce(
-          'Running command: {0}'.format(command),
-          level=INFO)
-      self.spawn(command.split(' '))
+        command = '{0} install --prefix {1} {1}'.format(
+            self.executable,
+            self.instance_dir,
+            )
+        self.announce(
+            'Running command: {0}'.format(command),
+            level=INFO)
+        self.spawn(command.split(' '))
